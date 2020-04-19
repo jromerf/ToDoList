6d1de2b1a84fe0bbd62e084a1f09f84735cc92d1
kriptorfeb <45903742+kriptorfeb@users.noreply.github.com>
2020-04-19 14:35:23 +0200

    19/04/20

---
+++ app/Models/Task.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model{
+  //Decimos el nombre de la tabla en phpmyadmin
+  protected $table = 'tasks';
+
+}
+
+ ?>
---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'vendor/autoload.php';
+use Illuminate\Database\Capsule\Manager as Capsule;
+require_once 'app/Models/Task.php';
+use App\Models\Task;
+
+use Relay\Relay;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'todo',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+
+$request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
+    $_SERVER,
+    $_GET,
+    $_POST,
+    $_COOKIE,
+    $_FILES
+);
+
+$loader = new Twig_Loader_Filesystem('.');
+$twig = new \Twig_Environment($loader, array(
+    'debug' => true,
+    'cache' => false,
+));
+
+$router = new Aura\Router\RouterContainer();
+$map = $router->getMap();
+
+$map->get('todo.list', '/', function($request)use($twig){
+  $tasks = Task::all();
+    $response = new Zend\Diactoros\Response\HtmlResponse($twig->render('template.twig', [
+        'tasks' => $tasks
+    ]));
+    return $response;
+});
+
+$map->post('todo.add','/add',function($request){
+    $data = $request->getParsedBody();
+    $task = new Task();
+    $task->description = $data['description'];
+    $task->save();
+
+    $response = new Zend\Diactoros\Response\RedirectResponse('/');
+    return $response;
+});
+
+$relay = new Relay([
+    new Middlewares\AuraRouter($router),
+    new Middlewares\RequestHandler()
+]);
+
+$response = $relay->handle($request);
+
+foreach ($response->getHeaders() as $name => $values) {
+    foreach ($values as $value) {
+        header(sprintf('%s: %s', $name, $value), false);
+    }
+}
+echo $response->getBody();
